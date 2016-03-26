54fc8d1e4410560b0c5c004c2c8a5e0a8c659dbd
SweetBroHellaCode <etmaron@gmail.com>
2016-03-26 17:51:30 -0400

    Updated project structure

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by sweetbrohellacode on 3/26/16.
+ */
+public class main {
+    public static void main (String[] args){
+
+    }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by sweetbrohellacode on 3/26/16.
+ */
+public class test {
+}
